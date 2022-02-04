16ab1f695997c6b3246e2d35f760857c7e129ebe
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2022-02-04 12:34:09 +0100

    login page added and spring security configured

--- src/main/java/main/config/SecurityConfig.java
+++ src/main/java/main/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package main.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -20,8 +21,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication().withUser("John").password(paswordEncoder().encode("admin")).roles("ADMIN");
-		
+		auth.inMemoryAuthentication().withUser("John").password(paswordEncoder().encode("admin")).roles("ADMIN","EMPLOYEE")
+		.and().withUser("Eric").password(paswordEncoder().encode("employee")).roles("EMPLOYEE")
+		.and().withUser("Michael").password(paswordEncoder().encode("client")).roles("CLIENT");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/","/login").permitAll()
+		.and().formLogin().defaultSuccessUrl("/").permitAll()
+		.and().logout().logoutSuccessUrl("/").invalidateHttpSession(true).permitAll();
 	}
 	
 	
